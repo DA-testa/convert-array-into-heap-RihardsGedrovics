421b057ad71a300b0c662d57e5693cf8624e9123
RihardsGedrovics <125249238+RihardsGedrovics@users.noreply.github.com>
2023-03-19 18:47:57 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,53 @@
-# python3
+def parse_input_user():
+    n_var = int(input())
+    data_var = list(map(int, input().strip().split(' ')))
+    return n_var, data_var
 
+def parse_input_file(file_name_var):
+    file_var = open(file_name_var, "r", -1, "utf-8")
+    n_var = int(file_var.readline().strip())
+    data_var = list(map(int, file_var.readline().strip().split(' ')))
+    return n_var, data_var
 
-def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
-    return swaps
-
+def build_heap(data_var):
+    n_var = len(data_var)
+    swaps_var = []
+    for i_var in range(n_var // 2, -1, -1):
+        top_var = i_var
+        while True:
+            left_var = 2 * top_var + 1
+            right_var = 2 * top_var + 2
+            if left_var < n_var and data_var[left_var] < data_var[top_var]:
+                top_var = left_var
+            if right_var < n_var and data_var[right_var] < data_var[top_var]:
+                top_var = right_var
+            if i_var != top_var:
+                data_var[i_var], data_var[top_var] = data_var[top_var], data_var[i_var]
+                swaps_var.append((i_var, top_var))
+                i_var = top_var
+            else:
+                break
+    return swaps_var
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    n_var = 0
+    input_data_var = []
+    try:
+        key_var = input().strip()
+        if (key_var.upper() == "I"):
+            n_var, input_data_var = parse_input_user()
+        elif (key_var.upper() == "F"):
+            file_name_var = input().strip()
+            if (file_name_var.lower() == "a"):
+                pass
+            n_var, input_data_var = parse_input_file("tests/" + file_name_var)
+    except:
+        pass
+    assert len(input_data_var) == n_var
+    swaps_var = build_heap(input_data_var)
+    print(len(swaps_var))
+    for i_var, j_var in swaps_var:
+        print(i_var, j_var)
 
 if __name__ == "__main__":
     main()
